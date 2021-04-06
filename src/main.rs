065fd9fd74e0137f0c89126ba4083980c132dc5c
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-04-05 20:18:05 -0400

    Display. Better.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::min, str::Chars};
+use std::cmp::max;
 
 fn main() {
     println!("Hello, world!");
@@ -8,7 +8,13 @@ pub fn levenshtein(left: &str, top: &str) -> Vec<Vec<usize>> {
     let left_of_table_nb_chars = left.len() + 1;
     let top_of_table_nb_chars = top.len() + 1;
 
-    let matrix = vec![vec![0; top_of_table_nb_chars]; left_of_table_nb_chars];
+    let mut matrix = vec![vec![0; top_of_table_nb_chars]; left_of_table_nb_chars];
+
+    for i in 0..matrix.len() {
+        for j in 0..matrix[i].len() {
+            matrix[i][j] = 1;
+        }
+    }
 
     matrix
 }
@@ -22,19 +28,23 @@ mod tests {
         let top = "acgccg";
         let result = levenshtein(left, top);
 
-        print!("[ ][⌀]");
+        print!("[ ][Ø]");
         for char in top.chars().into_iter() {
             print!("[{}]", char);
         }
         println!();
-        for i in 0..result.len() {
-            if i == 0 {
-                print!("[⌀]");
-            } else if i != 0 {
-                print!("[{}]", left.chars().nth(i - 1).unwrap());
-            }
+
+        print!("[Ø]");
+        for i in 0..result[0].len() {
+            print!("[{}]", result[0][i]);
+        }
+
+        println!();
+
+        for i in 1..result.len() {
+            print!("[{}]", left.chars().nth(i - 1).unwrap());
             for j in 0..result[i].len() {
-                print!("[{}]", result[i][j])
+                print!("[{}]", result[i - 1][j])
             }
             println!()
         }
